526b4839169ea3e598f6530ae811df7a56b2f082
fire1fly <danyseidev@gmail.com>
2023-08-06 02:11:24 +0800

    config: change translations on init

---
+++ src/shared/config/i18n/en.ts
@@ -0,0 +1,22 @@
+const en = {
+  translation: {
+    howToGet: 'How to get?',
+    connect: 'Connect',
+    pageTitle: "Don't miss the chance to get free coins through airdrop",
+    pageSubtitle: 'Join us and follow the simple steps to become a member',
+    get: 'Get',
+    howItWork: 'How does it work?',
+    steps: {
+      title: 'How do I get free coins?',
+      subtitle: 'Join us and follow the simple steps to become a member',
+      wallet: 'Connect your wallet',
+      facebook: 'Subscribe to us on Facebook',
+      twitter: 'Follow us on Twitter',
+    },
+    subscribe: 'Subscribe',
+    joinus: 'Join us',
+    copyright: 'All rights reserved. *Project name 2023',
+  },
+};
+
+export default en;
--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -1,21 +1,21 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import en from './en';
+import ru from './ru';
 
 i18n
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources: {
+      en, ru,
+    },
     fallbackLng: 'ru',
     debug: __IS_DEV__,
     interpolation: {
       escapeValue: false,
-    },
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
     },
     keySeparator: '.',
     react: {
