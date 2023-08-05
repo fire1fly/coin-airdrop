5f8de87024e4e79b9ec36e441120e35d5f9ea480
fire1fly <danyseidev@gmail.com>
2023-08-05 16:49:02 +0800

    feat: add LangSwitcher

---
+++ src/entities/LangSwitcher/model/languages.ts
@@ -0,0 +1,9 @@
+export interface ILanguage {
+  label: string;
+  value: string;
+}
+
+export const languages: ILanguage[] = [
+  { label: 'russian', value: 'ru-RU' },
+  { label: 'english', value: 'en-US' },
+];
